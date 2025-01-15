e1d4f2619dcc0e86501eec9f498910422ebc8e2a
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-01-16 02:01:18 +0600

    employee-worked-data-showing-on-worksheet-route

--- src/components/form/WorkSheetFrom.jsx
+++ src/components/form/WorkSheetFrom.jsx
@@ -1,96 +1,155 @@
-import React from "react";
-import { Button, DatePicker, Form, InputNumber, Select } from "antd";
+import React, { useState } from "react";
+import {
+  Button,
+  DatePicker,
+  Form,
+  InputNumber,
+  message,
+  notification,
+  Select,
+} from "antd";
 import dayjs from "dayjs";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import useAuth from "../hooks/useAuth";
+// toast notification config
+notification.config({
+  placement: "topRight",
+  top: 20,
+  bottom: 50,
+  duration: 3,
+});
+
+//
 const WorkSheetFrom = () => {
-  //
-  const onSubmit = (values) => {
-    console.log("Form Values:", values);
-    //
-    const extractedDate = dayjs(values?.todayDate?.$d);
-    // Format the date for the database
-    const formattedDate = extractedDate.format("YYYY-MM-DD");
-    console.log(extractedDate, formattedDate);
+  //custom axios
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  //handle min max value check
+  const [value, setValue] = useState(0);
+
+  const handleChange = (val) => {
+    if (val < 1 || val > 10) {
+      message.error("Value must be between 1 and 10!");
+      return;
+    }
+    setValue(val);
   };
 
-  //   error
+  //func for submit the form
+  const onSubmit = async (values) => {
+    //convert to date format
+    const extractedDate = dayjs(values?.workedDate?.$d);
+    // Format the date for the database
+    const formattedDate = extractedDate?.format("YYYY-MM-DD");
+    //
+    const workData = {
+      employeeEmail: user?.email,
+      taskName: values?.taskName,
+      workedHour: parseInt(values?.workedHour),
+      workedDate: formattedDate,
+    };
+    console.log(formattedDate, workData);
+    //send to db
+    try {
+      await axiosSecure.post("/daily-work", workData);
+      //
+      notification.success({
+        message: "Work Submited Successfully",
+        description: "Thanks for your todays hardwork",
+        placement: "topRight",
+      });
+    } catch (err) {
+      notification.error({
+        message: "Error Occurred",
+        description: `${err.response?.data?.message}`,
+        placement: "topRight",
+      });
+    }
+  };
+
+  //error
   const onFinishFailed = (errorInfo) => {
     console.error("Form submission failed:", errorInfo); // Handles validation errors
   };
 
   //
   return (
-      <div className="w-full">
-        <Form
-          name="basic"
-          onFinish={onSubmit}
-          onFinishFailed={onFinishFailed}
-          className="flex flex-wrap md:flex-nowrap justify-between items-start md:gap-6"
+    <div className="w-full">
+      <Form
+        name="basic"
+        onFinish={onSubmit}
+        onFinishFailed={onFinishFailed}
+        className="flex flex-wrap md:flex-nowrap justify-between items-start md:gap-6"
+      >
+        {/* select task */}
+        <Form.Item
+          name="taskName"
+          rules={[{ required: true, message: "Please select a task!" }]}
+          className="w-full md:w-3/12"
         >
-          {/* select task */}
-          <Form.Item
-            name="todayTask"
-            rules={[{ required: true, message: "Please select a task!" }]}
-            className="w-full md:w-3/12"
+          <Select
+            className="custom-select"
+            placeholder="Select a task"
+            style={{ width: "100%" }}
           >
-            <Select
-              className="custom-select"
-              placeholder="Select a task"
-              style={{ width: "100%" }}
-            >
-              <Select.Option value="sales">Sales</Select.Option>
-              <Select.Option value="support">Support</Select.Option>
-              <Select.Option value="content">Content</Select.Option>
-              <Select.Option value="paperWork">Paper work</Select.Option>
-              <Select.Option value="demo">Demo</Select.Option>
-            </Select>
-          </Form.Item>
-          {/* work hour */}
-          <Form.Item
-            name="workHour"
-            rules={[
-              {
-                required: true,
-                message: "Please select worked hour!",
-              },
-            ]}
-            className="w-full md:w-3/12"
+            <Select.Option value="sales">Sales</Select.Option>
+            <Select.Option value="support">Support</Select.Option>
+            <Select.Option value="content">Content</Select.Option>
+            <Select.Option value="paperWork">Paper work</Select.Option>
+            <Select.Option value="demo">Demo</Select.Option>
+          </Select>
+        </Form.Item>
+        {/* work hour */}
+        <Form.Item
+          name="workedHour"
+          rules={[
+            {
+              required: true,
+              message: "Please select worked hour!",
+            },
+          ]}
+          className="w-full md:w-3/12"
+        >
+          <InputNumber
+            min={1}
+            max={10}
+            value={value}
+            onChange={handleChange}
+            className="custom-input-number"
+            placeholder="Select worked hour"
+            type="number"
+            style={{ width: "100%" }}
+          />
+        </Form.Item>
+        {/* work data */}
+        <Form.Item
+          name="workedDate"
+          rules={[
+            {
+              required: true,
+              message: "Please select a date!",
+            },
+          ]}
+          className="w-full md:w-3/12"
+        >
+          <DatePicker
+            className="custom-date"
+            placeholder="Select worked Date"
+            style={{ width: "100%" }}
+          />
+        </Form.Item>
+        {/* submit button */}
+        <Form.Item className="w-full md:w-2/12">
+          <Button
+            className="custom-button"
+            htmlType="submit"
+            style={{ width: "100%" }}
           >
-            <InputNumber
-              className="custom-input-number"
-              placeholder="Select worked hour"
-              type="number"
-              style={{ width: "100%" }}
-            />
-          </Form.Item>
-          {/* work data */}
-          <Form.Item
-            name="todayDate"
-            rules={[
-              {
-                required: true,
-                message: "Please select a date!",
-              },
-            ]}
-            className="w-full md:w-3/12"
-          >
-            <DatePicker
-              className="custom-date"
-              placeholder="Select worked Date"
-              style={{ width: "100%" }}
-            />
-          </Form.Item>
-          {/* submit button */}
-          <Form.Item className="w-full md:w-2/12">
-            <Button
-              className="custom-button"
-              htmlType="submit"
-              style={{ width: "100%" }}
-            >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
--- src/components/table/WorkSheetTable.jsx
+++ src/components/table/WorkSheetTable.jsx
@@ -1,41 +1,33 @@
 import React from "react";
 import { Space, Table, Tag } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import useAuth from "../hooks/useAuth";
 
 const columns = [
   {
     title: "Tasks",
-    dataIndex: "name",
+    dataIndex: "taskName",
     key: "name",
    
   },
   {
     title: "Hours Worked",
-    dataIndex: "age",
+    dataIndex: "workedHour",
     key: "age",
   },
   {
     title: "Date",
-    dataIndex: "address",
+    dataIndex: "workedDate",
     key: "address",
   },
   {
-    title: "Tags",
-    key: "tags",
-    dataIndex: "tags",
-    render: (tags) => (
-      <span>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? "geekblue" : "green";
-          if (tag === "loser") {
-            color = "volcano";
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </span>
+    title: "Action",
+    key: "action",
+    render: () => (
+      <Space size="middle">
+        <a>Update</a>
+      </Space>
     ),
   },
   {
@@ -43,49 +35,42 @@ const columns = [
     key: "action",
     render: () => (
       <Space size="middle">
-        <a>Edit</a>
         <a>Delete</a>
       </Space>
     ),
   },
 ];
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
-  },
-];
+
 
 // work sheet table
 const WorkSheetTable = () => {
+// custom axios
+const axiosSecure = useAxiosSecure();
+const {user} = useAuth();
+// get data by tanstack query
+const {data:empWorkSheet = [] } = useQuery({
+  queryKey:["empWorkSheet", user?.email],
+  enabled:!!user?.email,
+  queryFn:async()=>{
+     const {data} = await axiosSecure(`/worksheet/${user?.email}`);
+     return data
+  }
+})
+
+
+
+
   //
   return (
     <>
       <Table
-       className="whitespace-nowrap"
+       className="whitespace-nowrap capitalize"
         columns={columns}
         pagination={{
           position: ["bottomRight"],
 
         }}
-        dataSource={data}
+        dataSource={empWorkSheet}
       />
     </>
   );
